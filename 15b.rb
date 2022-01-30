c9d947ea10621ae2d7d24f866332a45013b4fd3b
Jeremy Stanley <jeremy@instructure.com>
2022-01-29 17:32:56 -0700

    this kind of feels like cheating
    but the solution space is small enough to brute-force, so

---
+++ 15b.rb
@@ -0,0 +1,34 @@
+ingredients = {}
+calories = {}
+ARGF.each_line do |line|
+  ingredient, attrs = line.split(':')
+  ingredients[ingredient] = (attrs.split(',').map(&:split)).to_h.tap do |h|
+    h.transform_values! { |v| v.to_i }
+    calories[ingredient] = h.delete('calories')
+  end
+end
+pp ingredients
+
+require 'z3'
+opt = Z3::Optimize.new
+
+amounts = ingredients.keys.map { Z3::Int(_1) }
+properties = ingredients.first.last.keys.map { Z3::Int(_1) }
+score = Z3::Int("score")
+
+opt.assert(amounts.sum == 100)
+opt.assert(amounts.map { |amount| amount * calories[amount.to_s] }.sum == 500)
+
+properties.each do |property|
+  int_sum = Z3::Int("#{property.to_s}_int")
+  opt.assert(int_sum == amounts.map { |amount| amount * ingredients[amount.to_s][property.to_s] }.sum)
+  opt.assert(property == Z3.IfThenElse(int_sum > 0, int_sum, 0))
+end
+opt.assert(score == properties.inject(:*))
+opt.maximize(score)
+
+if opt.satisfiable?
+  puts opt.model.to_s
+else
+  puts "no solution :("
+end
