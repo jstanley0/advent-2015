429e63971a9b0595a6dc73e4ea999bed42ffe3bb
Jeremy Stanley <jeremy@instructure.com>
2022-02-04 14:37:58 -0700

    be a little smrtr about this
    since we want the smallest set that sums to the right weight,
    we can stop looking once we've found one
    
    which makes this take about a second instead of 40, without
    needing dynamic programming

--- 24b.rb
+++ 24b.rb
@@ -1,26 +1,19 @@
-def subset_sums(ints, target)
-  # meh. it's easier to wait 40 seconds than to figure out how to
-  # implement this using dynamic programming :P
-  (1..ints.size).map { |c| ints.combination(c).select { _1.sum == target } }.flatten(1)
+def subset_sums(ints, target, subrange = 1..ints.size)
+  subrange.map { |c| ints.combination(c).select { _1.sum == target } }.flatten(1)
 end
 
 weights = ARGF.each_line.map(&:to_i)
 raise "not divisible by 4" unless weights.sum % 4 == 0
 target = weights.sum / 4
-part1 = subset_sums(weights, target)
-print "#{part1.size} ways to build the first fourth..."
-
-# sort by size and then QE, so the first candidate where part 2 splits twice is good
-part1.sort_by! { |combo| [combo.size, combo.inject(:*)] }
-puts "sorted."
-
-part1.each do |combo1|
-  sub1 = weights - combo1
-  subset_sums(sub1, target).each do |combo2|
-    sub2 = sub1 - combo2
-    if subset_sums(sub2, target).any?
-      puts combo1.inject(:*)
-      exit
+(1..weights.size).each do |s|
+  subset_sums(weights, target, s..s).sort_by { _1.inject(:*) }.each do |combo1|
+    subset1 = weights - combo1
+    subset_sums(subset1, target).each do |combo2|
+      subset2 = subset1 - combo2
+      if subset_sums(subset2, target).any?
+        puts combo1.inject(:*)
+        exit
+      end
     end
   end
 end
