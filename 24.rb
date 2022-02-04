92266a84962c4e2dc6676a1f8733f29fa0d9a9fe
Jeremy Stanley <jeremy@instructure.com>
2022-02-04 14:37:58 -0700

    be a little smrtr about this
    since we want the smallest set that sums to the right weight,
    we can stop looking once we've found one
    
    which makes this take about a second instead of 40, without
    needing dynamic programming

--- 24.rb
+++ 24.rb
@@ -1,21 +1,15 @@
-def subset_sums(ints, target)
-  # todo write this better
-  (1..ints.size).map { |c| ints.combination(c).select { _1.sum == target } }.flatten(1)
+def subset_sums(ints, target, subrange = 1..ints.size)
+  subrange.map { |c| ints.combination(c).select { _1.sum == target } }.flatten(1)
 end
 
 weights = ARGF.each_line.map(&:to_i)
 raise "not divisible by 3" unless weights.sum % 3 == 0
 target = weights.sum / 3
-part1 = subset_sums(weights, target)
-print "#{part1.size} ways to build the first third..."
-
-# sort by size and then QE, so the first candidate where part 2 splits is good
-part1.sort_by! { |combo| [combo.size, combo.inject(:*)] }
-puts "sorted."
-
-part1.each do |combo1|
-  if subset_sums(weights - combo1, target).any?
-    puts combo1.inject(:*)
-    break
+(1..weights.size).each do |s|
+  subset_sums(weights, target, s..s).sort_by { _1.inject(:*) }.each do |combo|
+    if subset_sums(weights - combo, target).any?
+      puts combo.inject(:*)
+      exit
+    end
   end
 end
